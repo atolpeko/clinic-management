487c7ce4abc2688edf951f9f425301c3c824144d
Alexander Tolpeko <alextolpeko@gmail.com>
2022-03-22 09:42:11 +0300

    Docker integration

--- discovery-server/src/test/java/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/discoveryserver/DiscoveryServerApplicationTests.java
@@ -20,11 +20,11 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 @Tag("category.IntegrationTest")
-class DiscoveryServerApplicationTests {
+@SpringBootTest(properties = "spring.cloud.config.enabled=false")
+public class DiscoveryServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 }
