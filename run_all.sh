f08676a339b0fffdd453cc558f7fae65f0964b1c
Alexander Tolpeko <alextolpeko@gmail.com>
2022-03-22 19:49:34 +0300

    Enabled local run without docker

---
+++ run_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Author : Alexander Tolpeko
+
+mkdir logs
+
+echo "Starting config server"
+java -jar config-server/target/config-server-1.0-SNAPSHOT.jar > logs/config-server.log &
+sleep 20
+echo "Config server started"
+echo
+
+echo "Starting discovery server"
+java -jar discovery-server/target/discovery-server-1.0-SNAPSHOT.jar > logs/discovery-server.log &
+sleep 20
+echo "Discovery server started"
+echo
+
+echo "Starting microservices"
+java -jar api-gateway/target/api-gateway-1.0-SNAPSHOT.jar > logs/api-gateway.log &
+java -jar auth-server/target/auth-server-1.0-SNAPSHOT.jar > logs/auth-server.log &
+java -jar client-service/target/client-service-1.0-SNAPSHOT.jar > logs/client-service.log &
+java -jar clinic-service/target/clinic-service-1.0-SNAPSHOT.jar > logs/clinic-service.log &
+java -jar registration-service/target/registration-service-1.0-SNAPSHOT.jar > logs/registration-service.log &
+java -jar results-service/target/results-service-1.0-SNAPSHOT.jar > logs/results-service.log &
+sleep 70
+echo "System started"
+
+# wait for Ctrl-C
+( trap exit SIGINT ; read -r -d '' _ </dev/tty ) 
