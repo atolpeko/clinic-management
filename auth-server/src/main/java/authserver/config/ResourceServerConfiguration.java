a57e2b40cc9ed1e45254e0d9515bec9ddf777c3a
Alexander Tolpeko <alextolpeko@gmail.com>
2022-04-13 09:23:45 +0300

    Auth server: Actuator configured

---
+++ auth-server/src/main/java/authserver/config/ResourceServerConfiguration.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2002-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    private final TokenStore tokenStore;
+
+    @Autowired
+    public ResourceServerConfiguration(TokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .requestMatchers()
+                .mvcMatchers("/actuator/**")
+            .and()
+            .authorizeRequests()
+                .mvcMatchers("/actuator/**")
+                    .hasAnyAuthority("INTERNAL", "ADMIN")
+            .and()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER);
+    }
+}
